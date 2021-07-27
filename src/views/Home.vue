95251ebcf2e7514ae913171d49d049e7884d135c
Yvan <i@zhuyunfeng.com>
2021-07-27 21:15:20 +0800

    init

---
+++ src/views/Home.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="home">
+    <Header></Header>
+    <Main></Main>
+    <Footer/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+import Header from '@/components/Header.vue'
+import Main from '@/components/Main.vue'
+import Footer from '@/components/Footer.vue'
+
+import {defineComponent} from 'vue'
+export default defineComponent({
+  name: 'Home',
+  components: {
+    HelloWorld,
+    Header,
+    Main,
+    Footer
+  }
+})
+</script>
